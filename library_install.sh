71841eddd0d5e166ed1adf1a9c8dcdbdf556b2a1
k-oshima-chl <k-oshima@chlorina.co.jp>
2017-03-12 14:24:26 +0000

    library_install.shにコメントを追加

--- library_install.sh
+++ library_install.sh
@@ -1,6 +1,10 @@
+#apt-get updateで、サーバーのパッケージリストを取得する。
+#python-devでpythonの標準ライブラリを導入し、python-pipはpythonのパッケージ管理システムを導入
 sudo apt-get -y update
 sudo apt-get -y install python-dev
 sudo apt-get -y install python-pip
 
+#スクレイピングに必要なライブラリであるBeautifulSoupを導入。
+#MySQLを使用するためのライブラリ、MySQL-pythonを導入
 sudo pip install beautifulsoup4
 sudo pip install MySQL-python
